79d253080a6a5cd952f5e08e1242f65c4839b691
ligthman <jamka888@gmail.com>
2024-12-06 20:14:13 +0800

    jack

--- web/components/ui/icons/index.ts
+++ web/components/ui/icons/index.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 export * from "./LocationIcon";
 export * from "./GuestsIcon";
 export * from "./FilterIcon";
 export * from "./SearchIcon";
-=======
-export { BrandIcon } from "./BrandIcon";
->>>>>>> 24ce1b1 (jack)
