d7ed1fdfe8c9845a9d9b0b96ee707fc1f8c041a0
ligthman <jamka888@gmail.com>
2024-12-07 16:05:16 +0800

    conflict

--- web/components/pages/HomePage.tsx
+++ web/components/pages/HomePage.tsx
@@ -1,12 +1,22 @@
+<<<<<<< HEAD
 import { Hero } from "../features/homepage/Hero";
+=======
+import { LogoSection } from "../features/homepage/LogoSection";
+>>>>>>> 24ce1b1 (jack)
 import { Navbar } from "../features/homepage/Navbar";
 import { SinglePage } from "../features/singlePage/SinglePage";
 
 export default function HomePage() {
   return (
+<<<<<<< HEAD
     <div>
       <Navbar />
       <Hero />
+=======
+    <div className="w-screen flex flex-col justify-center">
+      <Navbar />
+      <LogoSection />
+>>>>>>> 24ce1b1 (jack)
     </div>
   );
 }
