f4a2f7c49eb68ccb8c85e2f1142c750df0bc204f
gereltbaatar <mglgerelt@gmail.com>
2024-12-07 19:11:52 +0800

    single page edit hiisen

--- web/components/ui/icons/index.ts
+++ web/components/ui/icons/index.ts
@@ -2,3 +2,5 @@ export * from "./LocationIcon";
 export * from "./GuestsIcon";
 export * from "./FilterIcon";
 export * from "./SearchIcon";
+export * from "./PeopleIcon";
+export * from "./SeatIcon";
