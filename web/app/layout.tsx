e8bc73ed7fb05e65ed10571f71c355779add99d1
Ганжаргал Гантөмөр <24LP0756@G401-CC0110.local>
2024-12-10 13:29:43 +0800

    footer hiisen

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { Navbar } from "@/components/layout/Navbar";
 import { LightTower } from "@/components/ui/icons/LightTower";
+import { Footer } from "@/components/footer/Footer";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
